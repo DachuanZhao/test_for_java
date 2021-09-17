8293505095cb8dac4e0d9a3bee47f23a9864e474
DachuanZhao <hnjyzdc@hotmail.com>
2021-09-17 11:59:01 +0800

    first commit

---
+++ src/main/java/com/kg/knowledge_graph/KnowledgeGraphApplication.java
@@ -0,0 +1,13 @@
+package com.kg.knowledge_graph;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnowledgeGraphApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KnowledgeGraphApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kg/knowledge_graph/KnowledgeGraphApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kg.knowledge_graph;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KnowledgeGraphApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
